c4107109628669dff162d1ea9d82b9dbe3ab5e30
Kyle Snavely <ksnavely@cloudant.com>
2013-09-19 12:56:40 -0400

    coffee_robot now is can has hypermedia

--- python/coffee_robot.py
+++ python/coffee_robot.py
@@ -6,36 +6,43 @@ I am a coffee robot, I take a work order and make coffee.
 Run me:
     python -m coffee_robot
 """
+import time
 
 import requests
 
-COLLECTION_URI = "http://10.0.12.137:1234/foo/bar"
+BASE_URI = "http://10.0.12.137:1234"
+COLLECTION_URI = "http://10.0.12.137:1234/coffee/input-queue"
 
-coffee_order = { 
-    'type': 'restfest-coffee',
-    'input': {
-        'drink-type' : 'mocha',
-        'size': 'small',
-        'addons' : [
-            { 'type' : 'half and half', 'amount' : '2oz' },
-            { 'type' : 'sugar',         'amount' : '1 cube' }
-        ]
-    }
-}
+def look_for_work():
+    rv = requests.get(COLLECTION_URI)
+    items = rv.json()['collection']['items']
+    print("  Found {0} jobs.".format(len(items)))
+    for i in items:
+        try:
+            do_work(i['href'])
+        except Exception as ex:
+            print("! An exception occurred: {0}".format(str(ex)))
 
-#def look_for_work():
-#    items = requests.get(COLLECTION_URI)
-#    for i in items:
-#        get_do_work(i.href)
-#
-#def get_do_work(URI):
-#    rv = requests.get(URI)
-#    wo = rv.json()
-#    coffee = make_coffee( wo )
-#    return coffee
+def do_work(RESOURCE_URI):
+    URI = BASE_URI + RESOURCE_URI
+    rv = requests.get(URI)
+    wo = rv.json()
 
-def make_coffee( work ):
-    if work.get('type', None) != 'restfest-coffee':
+    # Validate moar
+    validate_wo(wo)
+    
+    # Start work
+    requests.post(BASE_URI+wo['start'], data="Started brewing {0}".format(RESOURCE_URI))
+
+    print("    Brewing {0}.".format(RESOURCE_URI))
+    coffee = make_coffee( wo )
+    print(coffee)
+    
+    # Done
+    requests.post(BASE_URI+wo['complete'], coffee=coffee)
+
+def validate_wo( work ):
+    if work.get('type', None) != 'http://mogsie.com/2013/workflow/restfest-coffee':
         raise Exception('Work order must be type "restfest-coffee"')
     if work.get('input', None) is None:
         raise Exception('Work order must specify input')
@@ -47,8 +54,11 @@ def make_coffee( work ):
     if attrs.get('size', None) is None:
         raise Exception('size must be specified')
 
-    coffee = attrs['size'] + ' ' + attrs['drink-type']
+def make_coffee( work ):
+    attrs = work['input']
 
+    # Brew moar
+    coffee = attrs['size'] + ' ' + attrs['drink-type']
     if attrs.get('addons', None):
         for a in attrs['addons']:
             coffee = coffee + ' ' + a['amount'] + ' of ' + a['type']
@@ -56,4 +66,8 @@ def make_coffee( work ):
     return coffee
 
 if __name__ == '__main__':
-    print(make_coffee(coffee_order))
+    while True:
+        print("Looking for work...")
+        look_for_work()
+        print("No more work, sleeping...")
+        time.sleep(30)
